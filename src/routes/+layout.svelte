ee4a397623611d783aeb451e8c5230224b5bc684
Zayper <krasinpascha@yandex.ru>
2024-03-08 16:29:45 +0300

    downgrade

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Павел Красин</a>
         <div class="nav-links">
-            {#each nav as link} 
-            <a href={link.href} class="link">{link.title}</a>
-            {/each}
+            {#if nav.isShow}
+            <a href={nav.href} class="link">{nav.title}</a>
+            {/if}
         </div>
     </div>
 </nav>
