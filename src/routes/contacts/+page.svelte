5391e1f4a0cb674b5b85871e510235c4cd215f6d
Zayper <krasinpascha@yandex.ru>
2024-03-13 20:42:56 +0300

    5

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -4,3 +4,11 @@
     <div class="VK"><a href="https://vk.com/newzayper"><img src="https://img.icons8.com/?size=48&id=13977&format=png" width="120" height="120" alt="ВКонтакте"></a></div>
     <div class="Telegram"><a href="https://t.me/@newzayper"><img src="https://img.icons8.com/?size=48&id=63306&format=png" width="120" height="120" alt="Телеграм"></a></div>
 </div>
+
+<style>
+.contacts {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+}
+</style>
