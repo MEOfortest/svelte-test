dabf38e7c7bc383a0a0f5f83fc8b19175c22bf59
Zayper <krasinpascha@yandex.ru>
2024-03-13 20:53:31 +0300

    8

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,14 +1,27 @@
-<h1>Также вы можете связаться со мной следующими способами:</h1>
+<p>Также вы можете связаться со мной следующими способами:</p>
 
 <div class="contacts">
     <div class="VK"><a href="https://vk.com/newzayper"><img src="https://img.icons8.com/?size=48&id=13977&format=png" width="120" height="120" alt="ВКонтакте"></a></div>
     <div class="Telegram"><a href="https://t.me/@newzayper"><img src="https://img.icons8.com/?size=48&id=63306&format=png" width="120" height="120" alt="Телеграм"></a></div>
 </div>
 
-<style>
+<p>+7 (908) 164 09-44</p>
 
-.VK, .Telegram {
+<style>
+.VK {
     margin-left: 30px;
     display: inline-block;
 }
+
+.Telegram {
+    margin-left: 30px;
+    display: inline-block;
+    border-radius: 100%;
+}
+
+p {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: 20px;
+}
+
 </style>
