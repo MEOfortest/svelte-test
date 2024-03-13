ae5223e14d57119bf5e02c487764498f8d932d73
Zayper <krasinpascha@yandex.ru>
2024-03-13 20:38:06 +0300

    4

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,3 +1,6 @@
 <h1>Также вы можете связаться со мной следующими способами:</h1>
 
-<div class="Telegram"><a href="https://t.me/@newzayper"><img src="https://img.icons8.com/?size=48&id=13977&format=png" width="108" height="54" alt="Телеграм"></a></div>
+<div class="contacts">
+    <div class="VK"><a href="https://vk.com/newzayper"><img src="https://img.icons8.com/?size=48&id=13977&format=png" width="120" height="120" alt="ВКонтакте"></a></div>
+    <div class="Telegram"><a href="https://t.me/@newzayper"><img src="https://img.icons8.com/?size=48&id=63306&format=png" width="120" height="120" alt="Телеграм"></a></div>
+</div>
