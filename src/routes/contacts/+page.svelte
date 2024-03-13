b7eea583bdee397506580a70953345a0e48fdad2
Zayper <krasinpascha@yandex.ru>
2024-03-13 20:32:37 +0300

    7

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,1 +1,3 @@
-<h1>Пока что пусто)</h1>
+<h1>Также вы можете связаться со мной следующими способами:</h1>
+
+<div class="Telegram"><a href="https://t.me/@newzayper"><img src="https://img.icons8.com/?size=48&id=13977&format=png" width="108" height="54" alt="Телеграм"></a></div>
